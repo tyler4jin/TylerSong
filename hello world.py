0ef047298289d7bd1f4a501026bbef471bf8dd62
TylerSong <songtuanlai@gmail.com>
2023-07-16 15:33:37 +0800

    phthon

--- hello world.py
+++ hello world.py
@@ -1,1 +1,29 @@
 print("Hello, World!")
+# 基本资料形态 & 变数
+print("   /12-=/'/ll;k1")
+#数字
+70
+-87
+160.55
+#字串
+"这是字串"
+'小白你好'
+
+#布林值
+True
+False
+name = "小黑"
+age = 87
+is_male = True
+#1.变数的名称只能是英文OR数字OR_的组合
+#2.变数的开头不可以是数字
+#3.
+#清楚指令 windows cls  Mac clear
+print("有个人叫" + name)
+print(name + "今年87岁")
+print(name + "身高187公分")
+print(name + "讨厌自己87岁")
+name = 50
+print(name)
+name = True
+print(name)
